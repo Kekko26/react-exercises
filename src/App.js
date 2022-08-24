6b231b5a654ea4f239845a70ca1be5489662661c
Francesco Pio Acciarino <104023959+Kekko26@users.noreply.github.com>
2022-08-24 19:15:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Helloworld } from "./Helloworld";
 
 class App extends React.Component{
     render(){
+        return(
         <div>
             <h1>React app title test</h1>
             <Helloworld />
         </div>
+         )
     }
 }
