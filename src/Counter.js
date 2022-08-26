e6d66289a6caf8a832a3c5e698a41d6de932902c
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-08-26 18:19:57 +0200

    Exercise done

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CounterDisplay } from "./CounterDisplay";
 
 export class Counter extends React.Component{
     state = {
@@ -15,7 +16,7 @@ export class Counter extends React.Component{
         }, this.props.interval);
     }
     render(){
-        return <h1>{this.state.counter}</h1>
+        return <CounterDisplay counter={this.state.counter}/>
     }
 }
 
