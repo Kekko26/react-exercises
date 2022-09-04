ce808c4867cb6170e8be97b26ddde4da923483af
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-09-04 18:29:27 +0200

    Exercise done

--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export class CounterDisplay extends React.Component{
     render(){
-        return <h1>{this.props.counter}</h1>
+        const Mystyle = {
+            backgroundColor: 'yellow',
+            border: 'red 1px solid'
+        }
+
+        return <h1 style={Mystyle}>{this.props.counter}</h1>
     }
 }
