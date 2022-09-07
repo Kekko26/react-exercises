f96af004149096f0dde179c5e14b16c9aa06f298
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-09-07 19:08:33 +0200

    Exercise done

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { CounterButton } from "./CounterButton";
 
-export class ClickCounter extends React.Component{
-    state = {
-        count: 0
-    }
+export function ClickCounter(){
 
-    clickButtonEventHandler = () =>{
-        this.setState((state) =>{
-        return {count: state.count + 1}
+    const [count, setCounter] = useState(0)
+
+    function clickButtonEventHandler(){
+        setCounter(state => {
+            return state + 1
         })
     }
 
-    render(){
-        return (
-            <div>
-                <h3>Count of clicks: {this.state.count}</h3>
-                <CounterButton onClick={this.clickButtonEventHandler}/>
-            </div>
-        )
-    }
-
+    return(
+        <div>
+            <h3>Count of clicks: {count}</h3>
+            <CounterButton onClick={clickButtonEventHandler}/>
+        </div>
+    )
 }
+
+// export class ClickCounter extends React.Component{
+//     state = {
+//         count: 0
+//     }
+
+//     clickButtonEventHandler = () =>{
+//         this.setState((state) =>{
+//         return {count: state.count + 1}
+//         })
+//     }
+
+//     render(){
+//         return (
+//             <div>
+//                 <h3>Count of clicks: {this.state.count}</h3>
+//                 <CounterButton onClick={this.clickButtonEventHandler}/>
+//             </div>
+//         )
+//     }
+
+// }
