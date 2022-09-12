ff9d428ec478737d34b00158d4da1a38b30cdbaa
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-09-12 13:23:05 +0200

    Exercise done

---
+++ src/FilteredList.js
@@ -0,0 +1,16 @@
+import { useMemo } from "react";
+
+export function FilteredList({peopleList}){
+    
+    const adultList = useMemo(()=>{
+        return peopleList.filter(item => { return item.age > 18 
+        })
+    }, [peopleList])
+
+
+    return(
+        <div>
+            {adultList.map(item=>{return <div className="m-1 p-2 border-solid border-white-300 border-1"><h4>{item.name}</h4><p>Age: {item.age} - ID: {item.id}</p></div>})}
+        </div>
+    )
+}
