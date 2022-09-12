f6c81ab1ef5cf0bea030c552db4adf873bc9671a
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-09-12 19:15:01 +0200

    Exercise done

--- src/AppTwo.js
+++ src/AppTwo.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter"
 import { ShowGitHubUser } from "./ShowGitHubUser";
@@ -7,6 +7,9 @@ export function AppTwo(){
     
     return(
         <div>
+            <h3>Navigate to:</h3>
+            <p><Link to={"/"}>Home</Link> | <Link to={"counter"}>Counter</Link> | <Link to={"/users/kekko26"}>My Github</Link></p>
+
         <Routes>
             <Route path="/" element={<Welcome name={'Pio'}/>}/>
             <Route path="counter" element={<Counter/>}/>
