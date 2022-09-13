81c475a0df69eb2f1ce17bd500adc6550e0d7604
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-09-13 22:34:35 +0200

    Exercise done

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -3,23 +3,18 @@ import { useGitHubUser } from "./useGitHubUser";
 
 export function GitHubUser({username}){
 
-    const {data, error, loading} = useGitHubUser(username)
+    const {data, error, isLoading, mutate} = useGitHubUser(username)
 
-
-    // function handleFetch(){
-    //     fetchData(username)
-
-    // }
 
     return(
         <div className=" border-1 border-white-300 border-solid m-5 p-5">
             {/* <button onClick={handleFetch}>Fetch user data</button> */}
 
-            {loading && <div>Loading..</div>}
+            {isLoading && <div>Loading..</div>}
             
-            { !loading && error && <h3>User not found</h3>}
+            {error && <h3>User not found</h3>}
             
-            { !loading && data && <div>
+            {data && <div>
             <h3>{data.login}'s Profile informations</h3>
             <p><strong>Profile real name:</strong> {data.name ? data.name: 'this user has no real name'}</p>
             <p><strong>Profile bio:</strong> {data.bio ? data.bio : 'this user has no bio'}</p>
--- src/useGitHubUser.js
+++ src/useGitHubUser.js
@@ -1,41 +1,46 @@
-import {  useCallback, useEffect, useState } from "react"
+// import {  useCallback, useEffect, useState } from "react"
+import useSWR from "swr"
 
 export function useGitHubUser(username){
-    const [data, setData] = useState(false)
-    const [error, setError] = useState(false)
-    const [loading, setLoading] = useState(false)
+    const fetcher = url=> fetch(url).then(response => {if(response.ok){ return response.json() } else{throw new Error('An error has occurred in fetching')}}) 
+    const {data, error, isLoading = !data&&!error, mutate } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+ 
+//     const [data, setData] = useState(false)
+//     const [error, setError] = useState(false)
+//     const [loading, setLoading] = useState(false)
 
-const fetching = useCallback(async function fetchData(){
-        setLoading(true)
-        setError(false)
-try{
-    const fetching = await fetch(`https://api.github.com/users/${username}`)
-    if(fetching.ok){
-    const fetched = await fetching.json()
-    setData(fetched)
-    }
-    else{
-        throw new Error('User not found')
-    }
-}
-catch(e){
-    setError(true)
-    setData(false)
-}
-finally{setLoading(false)}
-    }, [username])
+// const fetching = useCallback(async function fetchData(){
+//         setLoading(true)
+//         setError(false)
+// try{
+//     const fetching = await fetch(`https://api.github.com/users/${username}`)
+//     if(fetching.ok){
+//     const fetched = await fetching.json()
+//     setData(fetched)
+//     }
+//     else{
+//         throw new Error('User not found')
+//     }
+// }
+// catch(e){
+//     setError(true)
+//     setData(false)
+// }
+// finally{setLoading(false)}
+//     }, [username])
     
 
-    useEffect(()=>{
+//     useEffect(()=>{
 
-    fetching()
+//     fetching()
 
-    },[fetching])
+//     },[fetching])
 
     return {
         data,
         error,
-        loading
+        isLoading,
+        mutate
     }
 
 
