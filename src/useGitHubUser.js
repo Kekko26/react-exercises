cbcc4a80bea6e2394640ed91cb0329953f12128e
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-09-13 22:44:55 +0200

    Exercise done

--- src/useGitHubUser.js
+++ src/useGitHubUser.js
@@ -3,7 +3,7 @@ import useSWR from "swr"
 
 export function useGitHubUser(username){
     const fetcher = url=> fetch(url).then(response => {if(response.ok){ return response.json() } else{throw new Error('An error has occurred in fetching')}}) 
-    const {data, error, isLoading = !data&&!error, mutate } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const {data, error, isLoading = !data&&!error, mutate } = useSWR(() => username!==null ? `https://api.github.com/users/${username}`: null, fetcher)
  
 //     const [data, setData] = useState(false)
 //     const [error, setError] = useState(false)
